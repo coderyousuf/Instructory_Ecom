72e0df1fd97b6da078c20d6b4c6cc1819f47751e
coderyousuf <yousuf2mail@gmail.com>
2022-08-28 07:19:10 +0600

    Auth

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+   public function loginPage(){
+    return view('backend.pages.auth.login');
+   }
+
+   public function login(Request $request){
+    $validated=$request->validate([
+        'email'=>'required|email',
+        'password'=>'required|string|min:5'
+    ]);
+
+    $credentials=[
+        'email'=>$request->email,
+        'password'=>$request->password,
+       ];
+
+       //login attempt if success then redirect dashboard
+       if(Auth::attempt($credentials, $request->filled('remember'))){
+        $request->session()->regenerate();
+        return redirect()->intended('admin/dashboard');
+       }
+
+       //with error message
+       return back()->withErrors([
+        'email'=>'Wrong Credentials found'
+       ])->onlyInput('email');
+   }
+
+   public function logout(Request $request){
+    Auth::logout();
+
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+
+    return redirect()->route('admin.login');
+   }
+
+
+}
